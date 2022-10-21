df4dc5db907eb4fd198a9c6cdd228d46826c6611
FIRST_NAME LAST_NAME <mohamed.hafez4050@gmail.com>
2022-10-21 21:54:54 +0200

    Add project files.

---
+++ CoreClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+		public void method1() 
+		{ 
+		
+		}
+		public void method2()
+		{
+
+		}
+		public void method3()
+		{
+
+		}
+		public void method4()
+		{
+
+		}
+		public void method5()
+		{
+
+		}
+	}
+}
---
+++ SWTestingProjectTemplate/UnitTest1.cs
@@ -0,0 +1,95 @@
+using NUnit.Framework;
+
+namespace SWTestingProjectTemplate
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Method1Test1()
+        {
+            
+        }
+        [Test]
+        public void Method1Test2()
+        {
+            
+        }
+        [Test]
+        public void Method1Test3()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Method2Test1()
+        {
+
+        }
+        [Test]
+        public void Method2Test2()
+        {
+
+        }
+        [Test]
+        public void Method2Test3()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Method3Test1()
+        {
+
+        }
+        [Test]
+        public void Method3Test2()
+        {
+
+        }
+        [Test]
+        public void Method3Test3()
+        {
+
+        }
+
+        [Test]
+        public void Method4Test1()
+        {
+
+        }
+        [Test]
+        public void Method4Test2()
+        {
+
+        }
+        [Test]
+        public void Method4Test3()
+        {
+
+        }
+
+        [Test]
+        public void Method5Test1()
+        {
+
+        }
+        [Test]
+        public void Method5Test2()
+        {
+
+        }
+        [Test]
+        public void Method5Test3()
+        {
+
+        }
+
+
+
+    }
+}
